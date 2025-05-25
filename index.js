8a340621178d83629049d74add329f1afb9e87b7
Dineshviyasar <dinesh.v@hyperready.tech>
2025-05-25 16:40:13 +0530

    2 nd commit

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const pdfRoutes = require("./routes/pdfRoutes");
 const connectDB = require("./config/db");
 
 const app = express();
+
+const allowedOrigins = ["http://localhost:3001", "http://localhost:5173"]; // React dev ports
+
+// 👇 Add CORS middleware
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      } else {
+        return callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 // Connect DB
